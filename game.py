afbf23e56817b3f20384281dc18fa6b4035e9823
km4.kim <kyoomok@naver.com>
2024-06-13 16:05:53 +0900

    [refactoring] length 점수 함수 구현

--- game.py
+++ game.py
@@ -3,13 +3,14 @@ class Game:
         self.MAX_SCORE_LENGTH = 60
 
     def guess(self, word_1, word_2):
+        return self.get_length_score(word_1, word_2)
+
+    def get_length_score(self, word_1, word_2):
         if len(word_1) == len(word_2):
             return self.MAX_SCORE_LENGTH
-
-        elif self.is_double_length(word_1, word_2):
+        if self.is_double_length(word_1, word_2):
             return 0
-        else:
-            return self.get_score_unmatched_length(word_1, word_2)
+        return self.get_score_unmatched_length(word_1, word_2)
 
     def get_score_unmatched_length(self, word_1, word_2):
         diff = abs(len(word_1) - len(word_2))
