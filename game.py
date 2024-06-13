066156298dd5cc59cca6b436966e1ab1e3b95b8f
km4.kim <kyoomok@naver.com>
2024-06-13 15:56:47 +0900

    [refactoring] 2배 길이 여부 체크 함수 추가

--- game.py
+++ game.py
@@ -2,9 +2,12 @@ class Game:
     def __init__(self):
         self.MAX_SCORE_LENGTH = 60
 
-    def guess(self, word_1, word2):
-        if len(word_1) == len(word2):
+    def guess(self, word_1, word_2):
+        if len(word_1) == len(word_2):
             return self.MAX_SCORE_LENGTH
 
-        if len(word_1) * 2 <= len(word2) or len(word_1) >= 2 * len(word2):
+        if self.is_double_length(word_1, word_2):
             return 0
+
+    def is_double_length(self, word_1, word_2):
+        return max(len(word_1), len(word_2)) >= min(len(word_1), len(word_2)) * 2
